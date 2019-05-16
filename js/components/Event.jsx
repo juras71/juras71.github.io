7422193811b6551c43d26fd8db8402d96823edf7
Kornel Kowalski <wujeq@gmail.com>
2019-05-16 15:51:11 +0200

    more styling

--- js/components/Event.jsx
+++ js/components/Event.jsx
@@ -84,6 +84,13 @@ export default class Event extends React.Component {
         evComments.classList.remove('hidden')
 
     }
+    closeComp (e){
+            (e.preventDefault()) ;
+            let compDiv=e.currentTarget.parentElement;
+            compDiv.classList.add('hidden');
+
+    }
+
     comment=(e)=>{
         let date = new Date();
         let newComment = [localStorage.getItem('avatar'),e.target.value,date.toLocaleTimeString(),date.toLocaleDateString(),localStorage.getItem('author')];
@@ -101,15 +108,13 @@ export default class Event extends React.Component {
         let date = new Date()
                 return (
             <div className="HolyGrail-content">
-
+                <div className="subNav">
+                    <button className="subNavBtn chat" onClick={this.showComments}><span className="tooltip">Dyskusja</span> </button>
+                    <button className="subNavBtn membersList" onClick={this.loadMembers}><span className="tooltip">Lista członków</span></button>
+                    <button className="subNavBtn joinEvent" onClick={this.joinEvent}><span className="tooltip">Dołącz do wydarzenia!</span></button>
+                </div>
                 <ul className="mainContent">
-                    <li>
-                        <div className="subNav">
-                            <button className="subNavBtn chat" onClick={this.showComments}><span className="tooltip">Dyskusja</span> </button>
-                            <button className="subNavBtn membersList" onClick={this.loadMembers}><span className="tooltip">Lista członków</span></button>
-                            <button className="subNavBtn joinEvent" onClick={this.joinEvent}><span className="tooltip">Dołącz do wydarzenia!</span></button>
-                        </div>
-                    </li>
+
 
                     <li className="evContainer">
                         <h2 className="eventName"> {eventInfo.eventName}<img className="sportLogo" alt={eventInfo.sport}
@@ -146,9 +151,9 @@ export default class Event extends React.Component {
                         {[...Array(parseInt(eventInfo.members[0]) - eventInfo.members.length + 1)].map((e, i) => <img
                             alt={eventInfo.sport} className="eventSlot" src={`./images/sports/${eventInfo.sport}.png`}
                             key={i}/>)}
-                        <div className="exitBtn" >
-
-                        </div>
+                        <button className="exitBtn"
+                                onClick={(e)=>this.closeComp(e)}>
+                        </button>
                     </li>
                     <li className="addComment">
                         <h2 className="commentsHeader">Twój komentarz:</h2>
@@ -156,7 +161,8 @@ export default class Event extends React.Component {
                             <label><textarea placeholder="Tu wpisz swój komentarz" onChange={this.comment}/></label>
                             <button type="submit">Dodaj komentarz</button>
                         </form>
-                        <div className="exitBtn" >
+                        <div className="exitBtn"
+                             onClick={(e)=>this.closeComp(e)}>
 
                         </div>
                     </li>
@@ -179,7 +185,8 @@ export default class Event extends React.Component {
                                    </div>
                             )
                         })}
-                        <div className="exitBtn" >
+                        <div className="exitBtn"
+                             onClick={(e)=>this.closeComp(e)}>
 
                         </div>
                     </li>
